16ff9d00beb05ad49e1dfb6366536340c5e932ef
jagathda <mail.jagath@gmail.com>
2024-08-27 19:26:57 +0300

    Added SQS queue with Stack class

--- lib/sqs_lambda_dynamo_db-cdk-stack.ts
+++ lib/sqs_lambda_dynamo_db-cdk-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
+//Stack Class
 export class SqsLambdaDynamoDbCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Define the SQS Queue
+    const myQueue = new sqs.Queue(this, 'MyQueue', {
+      queueName: 'my-queue-cdk',
+      visibilityTimeout: cdk.Duration.seconds(30)
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SqsLambdaDynamoDbCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
   }
 }
